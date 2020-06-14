95f39a38b8d73dbff38b3a264373b1b764afa155
lbakke <lbakke@nd.edu>
2020-06-14 12:41:20 -0500

    initial links layout

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -2,11 +2,55 @@ import React, { Component } from 'react'
 import '../App.css'; 
 import Footer from './shared/footer.jsx';
 
+import { Card } from 'antd';
+import OkGo from "../images/ok_go_piccolos.png";
+
+const { Meta } = Card;
+
 export default class Links extends Component {
     render() {
       return (
         <div>
-            <div className="container-w1">Coming soon: "useful links" page</div>
+            <div className="container" style={{"padding-top": "40px"}}>
+              <div className="row">
+                <div className="col-lg">
+                <a href="https://www.youtube.com/watch?v=4wRw9chUmu8" className="links-href">
+                  <Card
+                  hoverable
+                  className="links-href"
+                  cover={<img alt="example" src={OkGo} />}
+                  >
+                  <Meta title="Piccolos dance with OK Go" description="Watch the 2009 piccolos do the iconic 'Techno' 
+                  dance with the band OK Go." />
+                </Card>
+                </a>
+                </div>
+                <div className="col-lg">
+                  <Card
+                  hoverable
+                  className="links-href"
+                  cover={<img alt="example" src={OkGo} />}
+                  >
+                  <a href="https://www.youtube.com/watch?v=4wRw9chUmu8" className="links-href">
+                  <Meta title="Piccolos dance with OK Go" description="Watch the 2009 piccolos do the iconic 'Techno' 
+                  dance with the band OK Go." />
+                </a></Card>
+                </div>
+                <div className="col-lg">
+                  <Card
+                  hoverable
+                  className="links-href"
+                  cover={<img alt="example" src={OkGo} />}
+                  >
+                  <a href="https://www.youtube.com/watch?v=4wRw9chUmu8" className="links-href">
+                  <Meta title="Piccolos dance with OK Go" description="Watch the 2009 piccolos do the iconic 'Techno' 
+                  dance with the band OK Go." />
+                </a></Card>
+                </div>
+              </div>
+            </div>
+
+
             <Footer/>
         </div>
       )
--- src/components/members.jsx
+++ src/components/members.jsx
@@ -1,25 +1,29 @@
 import React, { Component } from 'react'
 import '../App.css'; 
+
 import Footer from './shared/footer.jsx';
 import FlippingCard from './shared/flipping-card.jsx';
+
+import { Divider } from 'antd';
+import 'antd/dist/antd.css';
 
 export default class Members extends Component {
     render() {
       return (
         <div>
-          <div className="members-text">
+          <div className="text-container">
             <h1>About our members:</h1>
             <p>The piccolos were 46 members strong in the 2019 season and while we will miss our seniors greatly, 
               we are looking forward to welcoming new members into our family! 
               Click on each photo to learn more about us!</p>
           </div>
             <div className="container">
+            <div className="members-divider-text"><Divider orientation="right">Seniors and Graduate Students</Divider></div>
               <div className="row">
                 <div className="col-lg">
                   <FlippingCard/>
                   <div className="members-caption">
                     <div>Lauren Bakke</div>
-                    <div>Senior</div>
                     <div>Section Leader</div>
                   </div>
                 </div>
@@ -27,7 +31,6 @@ export default class Members extends Component {
                   <FlippingCard/>
                   <div className="members-caption">
                     <div>Lauren Bakke</div>
-                    <div>Junior</div>
                     <div>Core Band Member</div>
                   </div>
                 </div>
@@ -35,7 +38,6 @@ export default class Members extends Component {
                   <FlippingCard/>
                   <div className="members-caption">
                     <div>Lauren Bakke</div>
-                    <div>Sophomore</div>
                     <div>Returning Band Member</div>
                   </div>
                 </div>
@@ -43,7 +45,6 @@ export default class Members extends Component {
                   <FlippingCard/>
                   <div className="members-caption">
                     <div>Lauren Bakke</div>
-                    <div>First Year Student</div>
                     <div>New Band Member</div>
                   </div>
                 </div>
@@ -62,6 +63,37 @@ export default class Members extends Component {
                   <FlippingCard/>
                 </div>
               </div>
+              <div className="text-container"><Divider orientation="right">Juniors</Divider></div>
+              <div className="row" style={{"padding-bottom": "40px"}}>
+              <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+              </div>
+              <div className="text-container"><Divider orientation="right">Sophomores</Divider></div>
+              <div className="row" style={{"padding-bottom": "40px"}}>
+              <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+              </div>
+              <div className="text-container"><Divider orientation="right">First Year Students</Divider></div>
               <div className="row" style={{"padding-bottom": "40px"}}>
               <div className="col-lg">
                   <FlippingCard/>
