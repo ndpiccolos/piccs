c63c2a6da18291358fec737edda80cbcad0f3b3c
lbakke <lbakke@nd.edu>
2020-06-13 23:32:12 -0500

    completing members page to add photo grid and captions

--- src/components/members.jsx
+++ src/components/members.jsx
@@ -1,12 +1,82 @@
 import React, { Component } from 'react'
 import '../App.css'; 
 import Footer from './shared/footer.jsx';
+import FlippingCard from './shared/flipping-card.jsx';
 
 export default class Members extends Component {
     render() {
       return (
         <div>
-            <div className="container-w1">Coming soon: members page</div>
+          <div className="members-text">
+            <h1>About our members:</h1>
+            <p>The piccolos were 46 members strong in the 2019 season and while we will miss our seniors greatly, 
+              we are looking forward to welcoming new members into our family! 
+              Click on each photo to learn more about us!</p>
+          </div>
+            <div className="container">
+              <div className="row">
+                <div className="col-lg">
+                  <FlippingCard/>
+                  <div className="members-caption">
+                    <div>Lauren Bakke</div>
+                    <div>Senior</div>
+                    <div>Section Leader</div>
+                  </div>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                  <div className="members-caption">
+                    <div>Lauren Bakke</div>
+                    <div>Junior</div>
+                    <div>Core Band Member</div>
+                  </div>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                  <div className="members-caption">
+                    <div>Lauren Bakke</div>
+                    <div>Sophomore</div>
+                    <div>Returning Band Member</div>
+                  </div>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                  <div className="members-caption">
+                    <div>Lauren Bakke</div>
+                    <div>First Year Student</div>
+                    <div>New Band Member</div>
+                  </div>
+                </div>
+              </div>
+              <div className="row" style={{"padding-bottom": "40px"}}>
+              <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+              </div>
+              <div className="row" style={{"padding-bottom": "40px"}}>
+              <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+                <div className="col-lg">
+                  <FlippingCard/>
+                </div>
+              </div>
+            </div>
             <Footer/>
         </div>
       )
---
+++ src/components/shared/flipping-card.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import '../../App.css'; 
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
+
+import headshot from "../../images/test_photo.png";
+
+var photoStyle = {
+    "display": "flex",
+    "align-items": "center",
+    "text-align": "center",
+    "justify-content": "center"
+}
+
+export default class About extends Component {
+    render() {
+      return (
+        <Flippy
+            flipOnHover={false} // default false
+            flipOnClick={true} // default false
+            flipDirection="horizontal" // horizontal or vertical
+            ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+            // if you pass isFlipped prop component will be controlled component.
+            // and other props, which will go to div
+        >
+            <FrontSide style={photoStyle}>
+              <img src={headshot} style={{"height": "200px", "width": "200px"}}/>
+            </FrontSide>
+            <BackSide style={{"font-size": "15px", "font-family": "RalewayLight"}}>
+            <div>Lauren is a computer science major from Minneapolis, MN! She is a senior and section leader this year and 
+                is super excited to be back in the bend! she loves her picco-fam and her montra is piccolos ride or die! </div>
+            </BackSide>
+        </Flippy>
+      )
+    }
+}
